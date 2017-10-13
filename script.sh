cff90ff8a1e6e7396738eb8c33fd45a899be8c54
transhap <fet.prashantsingh@gmail.com>
2017-10-13 06:32:51 +0530

    Runtime error handling

--- script.sh
+++ script.sh
@@ -1,13 +1,32 @@
 #!bin/bash
+#!bin/sh
 
-destFile=~/Desktop/Projects/repel/templateJava.java 
+if ! command -v java;
+then
+    echo "Java is either not installed or configured properly."
+    echo "Terminating shell"
+    exit
+fi    
+ 
+cleanup () {
+    rm -f ./replsh.java
+    if [ -f ./replsh.class ]
+    then
+        rm -f ./replsh.class
+    fi    
+    echo "\nExiting shell."
+    exit
+}
+
+touch ./replsh.java
+destFile=~/Desktop/Projects/repel/replsh.java 
 templateFile=~/Desktop/Projects/repel/template.txt
-progName=templateJava
+progName=replsh.java
 
 cat $templateFile > $destFile
 
-echo "Hello World!!!"
-echo "You should begin typing your code now. Press Ctrl-C to exit."
+echo "\nHello World!!!"
+echo "You should begin typing your code now. Press Ctrl-C to exit.\n"
 
 counter=0
 while [ $counter -le 1000 ]
@@ -16,32 +35,30 @@ do
     echo -n '>'
     counter=$((counter+=1))
     read input
+
+    trap cleanup INT TERM QUIT
+
     if [ -f "$destFile" ]
     then
-        head -n -2 templateJava.java > temp.txt; mv temp.txt templateJava.java
+        head -n -2 replsh.java > temp.txt; mv temp.txt replsh.java
         sed -i '/System.out/d' $destFile
 
-        if command -v java; 
-        then
-            echo "$input" >> "$destFile"
-            echo "}" >> "$destFile"
-            echo "}" >> "$destFile"
+        echo "$input" >> "$destFile"
+        echo "}" >> "$destFile"
+        echo "}" >> "$destFile"
             
-            if javac -classpath "." $progName;
+        if javac -classpath "." $progName;
+        then
+            if ! java replsh;
             then
-                if java templateJava;
-                then
-                    echo ""
-                else
-                    sed -i "/$input/d" $destFile
-                fi 
-            else
-                sed -i "/$input/d" $destFile           
-            fi
+                grep  -v "$input" "$destFile" > temp.txt; mv temp.txt replsh.java
+                #sed -i "/$input/d" $destFile
+            fi 
         else
-            echo "Java is either not installed or configured properly."
-            echo "Terminating shell"
-            exit    
+            sed -i "/$input/d" $destFile           
         fi
     fi    
 done    
+
+rm -f ./replsh.java
+exit
